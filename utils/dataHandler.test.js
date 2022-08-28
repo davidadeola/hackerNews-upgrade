c6f212023cbaf3c204e2ac43f18eb097a04e0fa4
davidadeola_ <98563443+davidadeola@users.noreply.github.com>
2022-08-28 02:22:01 +0100

    Installed testing dependencies

--- utils/dataHandler.test.js
+++ utils/dataHandler.test.js
@@ -1,7 +1,2 @@
 import { render, screen } from "@testing-library/react";
 import DataHandler from "./dataHandler";
-
-test("check if data is pulled", () => {
-  render(<DataHandler />);
-  const pulledData = screen.getBy;
-});
